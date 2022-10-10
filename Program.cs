09e360144c94ee3e0eb47960c31be6d87c7d7cf3
Igor <igor_parygin@mail.ru>
2022-10-11 01:28:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"m = {m}, n = {n}.");
+
+int[,] array = new int[m, n];
+
+CreateArray(array);
+
+WriteArray(array);
+
+Console.WriteLine();
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().Next(-99,99);
+    }
+  }
+}
+
+void WriteArray (int[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        int Number = array[i, j];
+        Console.Write(Number + " ");
+      }
+      Console.WriteLine();
+  }
+}
+
+Console.Write($"\nCреднее арифметическое:\n");
+for (int i = 0; i < n; i++)
+{
+  int arithmeticMean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    arithmeticMean += array[j, i];
+  }
+  arithmeticMean = arithmeticMean / m;
+  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
+}
